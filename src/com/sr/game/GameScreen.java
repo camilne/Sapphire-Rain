62194b26eb8fc61119b30599424bc5bc866423e8
Longarmx <cameronm213@gmail.com>
2017-05-22 21:06:30 -0600

    Created window structure

---
+++ src/com/sr/game/GameScreen.java
@@ -0,0 +1,28 @@
+package com.sr.game;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GameScreen extends JPanel {
+
+    private static final long serialVersionUID = -4652549849646030472L;
+
+    public GameScreen() {
+
+    }
+
+    public void init() {
+	// Empty
+    }
+
+    public void update() {
+	// Empty
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+	// Empty
+    }
+
+}
--- src/com/sr/main/Main.java
+++ src/com/sr/main/Main.java
@@ -1,9 +1,43 @@
 package com.sr.main;
 
+import javax.swing.JFrame;
+
+import com.sr.game.GameScreen;
+
 public class Main {
 
+    private final String title = "Sapphire Rain";
+    private final int width = 1280;
+    private final int height = 720;
+
+    private Main() {
+	final JFrame frame = new JFrame();
+	frame.setTitle(this.title);
+	frame.setSize(this.width, this.height);
+	frame.setLocationRelativeTo(null);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	final GameScreen game = new GameScreen();
+	game.setSize(this.width, this.height);
+	frame.add(game);
+
+	frame.setVisible(true);
+
+	Thread gameThread = new Thread(() -> {
+	    game.init();
+
+	    while (true) {
+		game.update();
+		game.repaint();
+	    }
+	});
+	gameThread.setDaemon(true);
+	gameThread.start();
+    }
+
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
-	System.out.println("Testing");
+	new Main();
     }
 
 }
