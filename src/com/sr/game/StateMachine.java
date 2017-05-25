ca8094c794c4f43012f55e1e44178c5c235574ca
Longarmx <cameronm213@gmail.com>
2017-05-24 22:30:19 -0600

    Added comments to code

--- src/com/sr/game/StateMachine.java
+++ src/com/sr/game/StateMachine.java
@@ -7,12 +7,18 @@ import javax.swing.JFrame;
 
 public class StateMachine {
 
+    // Lock for multi-threading synchronization of this object
     private static final Object key = new Object();
 
+    // Holds the singleton instance
     private static volatile StateMachine instance;
 
+    // Holds the stack of states
     private final ArrayDeque<State> stateStack;
+    // Holds the bindings between a state name and the state instance
     private final HashMap<String, State> stateMap;
+    // The main window of the application. Used to revalidate the window when
+    // state changes
     private JFrame mainWindow;
 
     private StateMachine(JFrame mainWindow) {
@@ -20,23 +26,27 @@ public class StateMachine {
 	this.stateMap = new HashMap<>();
 	this.mainWindow = mainWindow;
 
+	// Register the states under these names for later access
 	registerState("main-menu", new MainMenu());
 	registerState("game", new GameScreen());
     }
 
     public void update() {
+	// Updates the current state
 	if (getCurrentState() != null) {
 	    getCurrentState().update();
 	}
     }
 
     public void render() {
+	// Renders the current state
 	if (getCurrentState() != null) {
 	    getCurrentState().render();
 	}
     }
 
     public void registerState(final String stateName, final State state) {
+	// stateName nor state can be null
 	if (stateName == null) {
 	    throw new NullPointerException("Registering state name is null");
 	}
@@ -46,6 +56,9 @@ public class StateMachine {
 		    "Registering state is null with name: " + stateName);
 	}
 
+	// Register this state. Notice that a state type can be registered
+	// multiple times under different names, but not under the same name
+	// multiple times
 	this.stateMap.put(stateName, state);
     }
 
@@ -55,6 +68,7 @@ public class StateMachine {
 		    + ") does not exist in registered states");
 	}
 
+	// Adds the new state onto the state stack using the name lookup
 	pushState(this.stateMap.get(stateName));
     }
 
@@ -63,43 +77,56 @@ public class StateMachine {
 	    throw new NullPointerException("State is null");
 	}
 
+	// Initializes the new state
 	state.init();
 
 	if (this.mainWindow != null) {
 	    if (getCurrentState() != null) {
+		// Removes the old state from the JFrame if there is one
 		this.mainWindow.remove(getCurrentState());
 	    }
 
+	    // Adds the new state to the JFrame
 	    this.mainWindow.add(state);
+	    // Refreshes the JFrame to recognize the new state
 	    this.mainWindow.revalidate();
 	    this.mainWindow.repaint();
 	}
 
+	// Addes the new state to the stack (making it current)
 	this.stateStack.push(state);
     }
 
     public State popState() {
+	// The old state that is being popped
 	State resultState = this.stateStack.pop();
 
 	if (this.mainWindow != null) {
+	    // Removes the old state from the JFrame
 	    this.mainWindow.remove(resultState);
 
 	    if (getCurrentState() != null) {
+		// Adds the next state (now current) to the JFrame
 		this.mainWindow.add(getCurrentState());
 	    }
 
+	    // Refreshes the JFrame to recognize the new state
 	    this.mainWindow.revalidate();
 	    this.mainWindow.repaint();
 	}
 
+	// Returns the popped state
 	return resultState;
     }
 
     public State getCurrentState() {
+	// Returns null if there is nothing on the stack
 	if (this.stateStack.isEmpty()) {
 	    return null;
 	}
 
+	// Returns the top of the stack to get the current state (does not pop
+	// the state)
 	return this.stateStack.peek();
     }
 
@@ -107,12 +134,21 @@ public class StateMachine {
 	this.mainWindow = newFrame;
     }
 
+    /**
+     * Double-locking singleton pattern. Allows you to get a single, shared
+     * instance of this object across multiple threads
+     * 
+     * @return The state machine instance
+     */
     public static StateMachine getInstance() {
+	// Check if the instance is already initialized
 	if (instance != null) {
 	    return instance;
 	}
 
+	// Lock this block to the current thread to instantiate the instance
 	synchronized (key) {
+	    // Check if the instance has been initialized since the last check
 	    if (instance == null) {
 		instance = new StateMachine(null);
 	    }
--- src/com/sr/main/Main.java
+++ src/com/sr/main/Main.java
@@ -6,37 +6,52 @@ import com.sr.game.StateMachine;
 
 public class Main {
 
+    // The title of the application
     private final String title = "Sapphire Rain";
+    // The width of the window
     private final int width = 1280;
+    // The height of the window
     private final int height = 720;
 
     private Main() {
+	// Create main JFrame for the application
 	final JFrame frame = new JFrame();
 	frame.setTitle(this.title);
 	frame.setSize(this.width, this.height);
+	// Make window appear in the center of the screen
 	frame.setLocationRelativeTo(null);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.setResizable(false);
+	// Show the window
 	frame.setVisible(true);
 
+	// Thread for running the application
 	Thread gameThread = new Thread(() -> {
-	    StateMachine stateMachine = StateMachine.getInstance();
-	    stateMachine.setJFrame(frame);
+	    // Create a state machine to hold the state of the application
+		StateMachine stateMachine = StateMachine.getInstance();
+		stateMachine.setJFrame(frame);
 
-	    stateMachine.pushState("main-menu");
+		// Make the main menu the current state
+		stateMachine.pushState("main-menu");
 
-	    while (true) {
-		stateMachine.update();
-		stateMachine.render();
+		// Continue until the application closes
+		while (true) {
+		    // Update and render the current state
+		    stateMachine.update();
+		    stateMachine.render();
 
-		try {
-		    Thread.sleep(1000 / 60);
-		} catch (Exception e) {
-		    e.printStackTrace();
+		    // Slow the refresh rate to 60Hz (assuming the update and
+		    // render don't take any time)
+		    try {
+			Thread.sleep(1000 / 60);
+		    } catch (Exception e) {
+			e.printStackTrace();
+		    }
 		}
-	    }
-	});
+	    });
+	// Allow the application to stop when the window closes
 	gameThread.setDaemon(true);
+	// Start the runnable on a new thread
 	gameThread.start();
     }
 
