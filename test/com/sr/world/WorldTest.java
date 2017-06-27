217c73f3afc25456048bd530de4a77cdc6c8f510
Longarmx <cameronm213@gmail.com>
2017-06-26 23:06:33 -0600

    Add tests to improve code coverage

--- test/com/sr/world/WorldTest.java
+++ test/com/sr/world/WorldTest.java
@@ -2,7 +2,9 @@ package com.sr.world;
 
 import static org.junit.Assert.*;
 
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
 
 import org.junit.Test;
 
@@ -163,4 +165,318 @@ public class WorldTest {
 	assertEquals(y1 + y2, instance.getY(), EPSILON);
     }
 
+    @SuppressWarnings("static-method")
+    @Test
+    public void canUpdateEntitiesContainedInWorld() {
+	final World instance = new World();
+	final double offsetx = 10.0;
+	final double offsety = 2.0;
+
+	final Entity entity1 = new Entity() {
+
+	    @Override
+	    public void update() {
+		this.x += offsetx;
+		this.y += offsety;
+	    }
+
+	    @Override
+	    public void render(final Graphics g) {
+		// Empty
+	    }
+
+	};
+	instance.addEntity(entity1);
+
+	final Entity entity2 = new Entity() {
+
+	    @Override
+	    public void update() {
+		this.x -= offsetx;
+		this.y -= offsety;
+	    }
+
+	    @Override
+	    public void render(final Graphics g) {
+		// Empty
+	    }
+
+	};
+	instance.addEntity(entity2);
+
+	instance.update();
+
+	assertEquals(offsetx, entity1.getX(), EPSILON);
+	assertEquals(offsety, entity1.getY(), EPSILON);
+
+	assertEquals(-offsetx, entity2.getX(), EPSILON);
+	assertEquals(-offsety, entity2.getY(), EPSILON);
+    }
+
+    @SuppressWarnings("static-method")
+    @Test
+    public void canRenderEntitiesContainedInWorld() {
+	final World instance = new World();
+	final double offsetx = 10.0;
+	final double offsety = 2.0;
+
+	final Entity entity1 = new Entity() {
+
+	    @Override
+	    public void update() {
+		this.x += offsetx;
+		this.y += offsety;
+	    }
+
+	    @Override
+	    public void render(final Graphics g) {
+		// Empty
+	    }
+
+	};
+	instance.addEntity(entity1);
+
+	final Entity entity2 = new Entity() {
+
+	    @Override
+	    public void update() {
+		this.x -= offsetx;
+		this.y -= offsety;
+	    }
+
+	    @Override
+	    public void render(final Graphics g) {
+		// Empty
+	    }
+
+	};
+	instance.addEntity(entity2);
+
+	final Graphics dummyGraphics = new Graphics() {
+
+	    @Override
+	    public Graphics create() {
+		return null;
+
+	    }
+
+	    @Override
+	    public void translate(final int x, final int y) {
+		// Empty
+	    }
+
+	    @Override
+	    public Color getColor() {
+		return null;
+
+	    }
+
+	    @Override
+	    public void setColor(final Color c) {
+		// Empty
+	    }
+
+	    @Override
+	    public void setPaintMode() {
+		// Empty
+	    }
+
+	    @Override
+	    public void setXORMode(final Color c1) {
+		// Empty
+	    }
+
+	    @Override
+	    public Font getFont() {
+		return null;
+
+	    }
+
+	    @Override
+	    public void setFont(final Font font) {
+		// Empty
+	    }
+
+	    @Override
+	    public FontMetrics getFontMetrics(final Font f) {
+		return null;
+
+	    }
+
+	    @Override
+	    public Rectangle getClipBounds() {
+		return null;
+
+	    }
+
+	    @Override
+	    public void clipRect(final int x, final int y, final int width,
+		    final int height) {
+		// Empty
+	    }
+
+	    @Override
+	    public void setClip(final int x, final int y, final int width,
+		    final int height) {
+		// Empty
+	    }
+
+	    @Override
+	    public Shape getClip() {
+		return null;
+
+	    }
+
+	    @Override
+	    public void setClip(final Shape clip) {
+		// Empty
+	    }
+
+	    @Override
+	    public void copyArea(final int x, final int y, final int width,
+		    final int height, final int dx, final int dy) {
+		// Empty
+	    }
+
+	    @Override
+	    public void drawLine(final int x1, final int y1, final int x2,
+		    final int y2) {
+		// Empty
+	    }
+
+	    @Override
+	    public void fillRect(final int x, final int y, final int width,
+		    final int height) {
+		// Empty
+	    }
+
+	    @Override
+	    public void clearRect(final int x, final int y, final int width,
+		    final int height) {
+		// Empty
+	    }
+
+	    @Override
+	    public void drawRoundRect(final int x, final int y,
+		    final int width, final int height, final int arcWidth,
+		    final int arcHeight) {
+		// Empty
+	    }
+
+	    @Override
+	    public void fillRoundRect(final int x, final int y,
+		    final int width, final int height, final int arcWidth,
+		    final int arcHeight) {
+		// Empty
+	    }
+
+	    @Override
+	    public void drawOval(final int x, final int y, final int width,
+		    final int height) {
+		// Empty
+	    }
+
+	    @Override
+	    public void fillOval(final int x, final int y, final int width,
+		    final int height) {
+		// Empty
+	    }
+
+	    @Override
+	    public void drawArc(final int x, final int y, final int width,
+		    final int height, final int startAngle, final int arcAngle) {
+		// Empty
+	    }
+
+	    @Override
+	    public void fillArc(final int x, final int y, final int width,
+		    final int height, final int startAngle, final int arcAngle) {
+		// Empty
+	    }
+
+	    @Override
+	    public void drawPolyline(final int[] xPoints, final int[] yPoints,
+		    final int nPoints) {
+		// Empty
+	    }
+
+	    @Override
+	    public void drawPolygon(final int[] xPoints, final int[] yPoints,
+		    final int nPoints) {
+		// Empty
+	    }
+
+	    @Override
+	    public void fillPolygon(final int[] xPoints, final int[] yPoints,
+		    final int nPoints) {
+		// Empty
+	    }
+
+	    @Override
+	    public void drawString(final String str, final int x, final int y) {
+		// Empty
+	    }
+
+	    @Override
+	    public void drawString(final AttributedCharacterIterator iterator,
+		    final int x, final int y) {
+		// Empty
+	    }
+
+	    @Override
+	    public boolean drawImage(final Image img, final int x, final int y,
+		    final ImageObserver observer) {
+		return false;
+
+	    }
+
+	    @Override
+	    public boolean drawImage(final Image img, final int x, final int y,
+		    final int width, final int height,
+		    final ImageObserver observer) {
+		return false;
+
+	    }
+
+	    @Override
+	    public boolean drawImage(final Image img, final int x, final int y,
+		    final Color bgcolor, final ImageObserver observer) {
+		return false;
+
+	    }
+
+	    @Override
+	    public boolean drawImage(final Image img, final int x, final int y,
+		    final int width, final int height, final Color bgcolor,
+		    final ImageObserver observer) {
+		return false;
+
+	    }
+
+	    @Override
+	    public boolean drawImage(final Image img, final int dx1,
+		    final int dy1, final int dx2, final int dy2, final int sx1,
+		    final int sy1, final int sx2, final int sy2,
+		    final ImageObserver observer) {
+		return false;
+
+	    }
+
+	    @Override
+	    public boolean drawImage(final Image img, final int dx1,
+		    final int dy1, final int dx2, final int dy2, final int sx1,
+		    final int sy1, final int sx2, final int sy2,
+		    final Color bgcolor, final ImageObserver observer) {
+		return false;
+
+	    }
+
+	    @Override
+	    public void dispose() {
+		// Empty
+	    }
+
+	};
+	instance.render(dummyGraphics);
+    }
 }
