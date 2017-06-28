58def1e89a7d570c3fde1ac4c583d7b0bc58173e
Unknown <cameronm213@gmail.com>
2017-06-27 23:30:29 -0600

    Increase coverage of StateMachine tests

--- test/com/sr/game/StateMachineTest.java
+++ test/com/sr/game/StateMachineTest.java
@@ -2,6 +2,8 @@ package com.sr.game;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.NoSuchElementException;
+
 import javax.swing.JFrame;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -45,6 +47,18 @@ public class StateMachineTest {
     }
 
     @Test
+    public void throwsNullPointerExceptionWhenNullNameIsRegistered() {
+	assertThrows(NullPointerException.class,
+		() -> this.instance.registerState(null, this.emptyState));
+    }
+
+    @Test
+    public void throwsNullPointerExceptionWhenNullStateIsRegisteredWithName() {
+	assertThrows(NullPointerException.class,
+		() -> this.instance.registerState("test", null));
+    }
+
+    @Test
     public void canAddStateToStackByName() {
 	this.instance.registerState("test", this.emptyState);
 	this.instance.pushState("test");
@@ -57,6 +71,12 @@ public class StateMachineTest {
 	this.instance.registerState("test", this.emptyState);
 	assertThrows(IllegalArgumentException.class,
 		() -> this.instance.pushState("does-not-exist"));
+    }
+
+    @Test
+    public void throwsNullPointerExceptionWhenNullStateIsPushed() {
+	assertThrows(NullPointerException.class,
+		() -> this.instance.pushState((State) null));
     }
 
     @Test
@@ -103,6 +123,44 @@ public class StateMachineTest {
     }
 
     @Test
+    public void canAddStateToStackByInstanceWhenJFrameIsNull() {
+	this.instance.setJFrame(null);
+	this.instance.pushState(this.emptyState);
+
+	assertEquals(this.emptyState, this.instance.getCurrentState());
+    }
+
+    @Test
+    public void canAddMultipleStatesToStack() {
+	this.instance.pushState(this.emptyState);
+
+	final State state = new State() {
+
+	    private static final long serialVersionUID = 1L;
+
+	    @Override
+	    public void init() {
+		// Empty
+	    }
+
+	    @Override
+	    public void update() {
+		// Empty
+	    }
+
+	    @Override
+	    public void render() {
+		// Empty
+	    }
+
+	};
+
+	this.instance.pushState(state);
+
+	assertEquals(state, this.instance.getCurrentState());
+    }
+
+    @Test
     public void canGetCurrentState() {
 	this.instance.registerState("test", this.emptyState);
 	this.instance.pushState("test");
@@ -125,9 +183,74 @@ public class StateMachineTest {
     }
 
     @Test
+    public void throwsNoSuchElementExceptionWhenEmptyStackIsPopped() {
+	assertThrows(NoSuchElementException.class,
+		() -> this.instance.popState());
+    }
+
+    @Test
+    public void returnsPoppedState() {
+	this.instance.pushState(this.emptyState);
+
+	final State state = new State() {
+
+	    private static final long serialVersionUID = 1L;
+
+	    @Override
+	    public void init() {
+		// Empty
+	    }
+
+	    @Override
+	    public void update() {
+		// Empty
+	    }
+
+	    @Override
+	    public void render() {
+		// Empty
+	    }
+
+	};
+	this.instance.pushState(state);
+
+	assertEquals(state, this.instance.popState());
+	assertEquals(this.emptyState, this.instance.popState());
+    }
+
+    @Test
+    public void canPopStateWithNullJFrame() {
+	this.instance.setJFrame(null);
+	this.instance.pushState(this.emptyState);
+
+	assertEquals(this.emptyState, this.instance.popState());
+    }
+
+    @Test
     public void canSetJFrame() {
 	final JFrame dummyJFrame = new JFrame();
 	this.instance.setJFrame(dummyJFrame);
     }
 
+    @Test
+    public void canUpdateEmptyStateMachine() {
+	this.instance.update();
+    }
+
+    @Test
+    public void canUpdateStateMachine() {
+	this.instance.pushState(this.emptyState);
+	this.instance.update();
+    }
+
+    @Test
+    public void canRenderEmptyStateMachine() {
+	this.instance.render();
+    }
+
+    @Test
+    public void canRenderStateMachine() {
+	this.instance.pushState(this.emptyState);
+	this.instance.render();
+    }
 }
